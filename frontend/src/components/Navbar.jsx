277a2cbce51be4d78b1fc35495bcde0e04ba1770
Sounak Banerjee <sounakbanerjee28@gmail.com>
2026-05-28 02:03:51 +0530

    Updated files

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,15 @@
+import { useLocation } from "react-router-dom";
+
 export default function Navbar() {
+
+  const location = useLocation();
+
+  const navStyle = (path) =>
+    `px-4 py-2 rounded-lg transition duration-300 ${
+      location.pathname === path
+        ? "bg-white/25 backdrop-blur-sm font-semibold"
+        : "hover:bg-white/10"
+    }`;
 
   return (
     <div className="bg-orange-500 text-white px-10 py-5 flex justify-between items-center shadow-lg">
@@ -7,15 +18,17 @@ export default function Navbar() {
         RePlate
       </h1>
 
-      <div className="space-x-6 text-lg">
+      <div className="space-x-4 text-lg flex">
 
-        <a href="/">Home</a>
+        <a href="/" className={navStyle("/")}>
+          Home
+        </a>
 
-        <a href="/upload">
+        <a href="/upload" className={navStyle("/upload")}>
           Upload
         </a>
 
-        <a href="/ngo">
+        <a href="/ngo" className={navStyle("/ngo")}>
           NGO Dashboard
         </a>
 
